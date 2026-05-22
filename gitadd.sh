825f0eb56b172726a3a145896367fd216fee3612
Andrey A. Moiseenko <ip@maasoft.ru>
2026-05-22 14:01:17 +0300

    ~

--- gitadd.sh
+++ gitadd.sh
@@ -5,7 +5,7 @@ git add archiver/*.h archiver/*.cpp
 git add wwwcgi/*.h wwwcgi/*.cpp
 git add unix/*.h unix/*.cpp
 git add WIN32/*.h WIN32/*.cpp
-git add *hpp gitadd.sh Licence.txt json.png
+git add *hpp gitadd.sh Licence.txt .github
 git add ToolsLib.vcxproj ToolsLib.vcxproj.filters ToolsLib.pro Makefile Makefile1
 
 if [ x"$1" = "x" ]; then
